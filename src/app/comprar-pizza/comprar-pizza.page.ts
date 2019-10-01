69ae6328ad0c32e15b30511aa84092c8ccb654b7
Andrew Ribeiro <andrew.ribeiro@etec.sp.gov.br>
2019-09-30 22:22:59 -0300

    Implementação de tela compra com divisão dinamica

--- src/app/comprar-pizza/comprar-pizza.page.ts
+++ src/app/comprar-pizza/comprar-pizza.page.ts
@@ -9,13 +9,49 @@ import { ActivatedRoute } from '@angular/router';
 export class ComprarPizzaPage implements OnInit {
 
   idPizza;
-  constructor(private activatedRoute:ActivatedRoute) { }
+  nomePizza;
+  descricaoPizza;
+  precoPizza;
+  divisor = 1;
+  precoPizzaFixo;
+
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
     this.idPizza = this.activatedRoute.snapshot.params.id
+    let pizzaString = localStorage.getItem(this.idPizza)
+    let pizzaObjeto = JSON.parse(pizzaString)
+    this.nomePizza = pizzaObjeto.nomePizza
+    this.descricaoPizza = pizzaObjeto.descricaoPizza
+    this.precoPizza = pizzaObjeto.precoPizza
+    this.precoPizzaFixo = this.precoPizza
+
   }
+
+  incrementar() {
+    this.divisor++
+    this.atualizarPreco()
+  }
+
+  decrementar() {
+    if (this.divisor > 1) {
+      this.divisor--
+      this.atualizarPreco()
+    }
+  }
+
+  atualizarPreco() {
+    this.precoPizza = (parseFloat(this.precoPizzaFixo)
+      / this.divisor).toFixed(2)
+  }
+
+  atualizarComRange(){
+    this.atualizarPreco()
+  }
+
 
 }
